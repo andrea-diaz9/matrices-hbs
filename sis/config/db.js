4bf6a48bb4fa60f4c237035e81fc3a195b74a378
andrea-diaz9 <andrea.diaz@prointernet.mx>
2025-05-14 10:39:56 -0600

    reduccion de lineas function

--- sis/config/db.js
+++ sis/config/db.js
@@ -1,37 +1,25 @@
 const sql = require("mssql");
 
+const config = {
+  user: "sa", 
+  password: "prointernet", 
+  server: "DESKTOP-HUMS825", 
+  database: "calculadoraMatrices", 
+  options: {
+    encrypt: false, 
+    trustServerCertificate: true, 
+  }
+};
 
-  const config = {
-    user: "sa", 
-    password: "prointernet", 
-    server: "DESKTOP-HUMS825", 
-    database: "calculadoraMatrices", 
-    options: {
-      encrypt: false, 
-      trustServerCertificate: true, 
-    }
-  };
-
-  let pool
-
-  async function conexion_db (){
-    try {
-      if(!pool) {
-        pool = await sql.connect(config)
-        .then(async pool => {
-            console.log('Conexión a la base de datos exitosa')
-            /* const mostrarLista = await pool.request().query('select idOp, fechaOp, tipoOp, matrizA, matrizB, resultado from operaciones')
-            console.table(mostrarLista.recordset) */
-            /* console.table(mostrarLista.recordset) */ //es para que se vea en tabla en la consola
-          })
-        }
-        return pool;
-        
-      } catch (error) {
-        console.log('Error de conexion a la base de datos',error)
-        throw error
-      }
-    }
+async function conexion_db() {
+  try {
+    const pool = await sql.connect(config);
+    return pool;  
+  } catch (err) {
+    console.error('Error al conectar a la base de datos:', err);
+    throw err;
+  }
+}
     
 module.exports={
   conexion_db,
