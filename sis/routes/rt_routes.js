34be081b5ee4be9b91e678d96ab7c58b51027751
andrea-diaz9 <andrea.diaz@prointernet.mx>
2025-05-14 10:40:43 -0600

    añadi 'mssql', .post ya funciona

--- sis/routes/rt_routes.js
+++ sis/routes/rt_routes.js
@@ -1,31 +1,23 @@
 const express = require('express') 
 const { conexion_db } = require('../config/DB.JS')
 const router = express.Router() 
-//const inicio = require('../controllers/ctrl_inicio') 
-
-
+const sql = require("mssql");
 
 // Inicio 
- router.post('/opera',   async (req, res) => {
-    console.log('hoal')
-
-    console.log(req.body)
-    const { operacionJSON, matrizAjson, matrizBjson, resultadoJSON } = req.body
-
-     try {
-         const pool = await conexion_db()
-         const resultado = await pool.request()
-         .input('tipoOp', sql.NVarChar, operacionJSON)
-         .input('matrizA', sql.NVarChar, matrizAjson)
-         .input('matrizB', sql.NVarChar, matrizBjson)
-         .input('resultado', sql.NVarChar, resultadoJSON)
-         .query(`
-             INSERT INTO operaciones (tipoOp, matrizA, matrizB, resultado)
-             VALUES (@tipoOp, @matrizA, @matrizB, @resultado)`)
-        console.log(resultado)
+router.post('/opera',   async (req, res) => {
+    const { matrizAjson, matrizBjson,  operacionJSON, resultadoJSON } = req.body
+    try {
+        let  pool = await conexion_db()
+        const resultado = await pool.request()
+        .input('matrizA', sql.NVarChar, matrizAjson)
+        .input('matrizB', sql.NVarChar, matrizBjson)
+        .input('tipoOp', sql.NVarChar, operacionJSON)
+        .input('resultado', sql.NVarChar, resultadoJSON)
+        .query(`INSERT INTO operaciones ( matrizA, matrizB, tipoOp, resultado)`+
+        `VALUES ( @matrizA, @matrizB, @tipoOp,@resultado)`)
         res.json({resultado})
-     } catch (error) {
-         console.log('Error al guardar la operacion.', error)
+    } catch (error) {
+        console.log('Error al guardar la operacion.', error)
     } 
 })
 
